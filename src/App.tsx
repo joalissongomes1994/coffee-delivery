03e4cdd5c29b81c5b507a465641c2eb18478963d
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-18 12:02:44 -0300

    feat: Adding routing component in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { lightTheme } from './styles/themes/light'
 
 export function App() {
   return (
     <ThemeProvider theme={lightTheme}>
-      <div>
-        <h1>Hello, Coffee Delivery</h1>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )

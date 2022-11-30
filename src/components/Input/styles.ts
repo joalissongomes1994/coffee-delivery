415c25b7691fc57802b082364b318adfe7605194
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-30 16:57:28 -0300

    feat: Updated Input component style

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -9,7 +9,7 @@ export const InputContainer = styled.input`
   color: ${(props) => props.theme.color['base-text']};
 
   font-weight: 400;
-  font-size: 14px;
+  font-size: 0.875rem;
   line-height: 1.3;
 
   &::placeholder {

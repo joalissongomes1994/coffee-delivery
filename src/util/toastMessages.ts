a8f7b539813a75f117ad008649e7dd5f287baf02
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-12-01 23:32:17 -0300

    feat: toastMessages method created

---
+++ src/util/toastMessages.ts
@@ -0,0 +1,27 @@
+import { toast } from 'react-toastify'
+
+export function successMessage(message: string) {
+  toast.success(message, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  })
+}
+
+export function errorMessage(message: string) {
+  toast.error(message, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  })
+}

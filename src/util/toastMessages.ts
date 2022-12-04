b28453390756056f945f434f69aaf117573b9874
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-12-04 18:29:38 -0300

    feat: toastMessages method updated

--- src/util/toastMessages.ts
+++ src/util/toastMessages.ts
@@ -1,27 +1,24 @@
 import { toast } from 'react-toastify'
 
+const props = {
+  position: 'top-right',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'light',
+} as const
+
 export function successMessage(message: string) {
-  toast.success(message, {
-    position: 'top-right',
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'light',
-  })
+  toast.success(message, props)
 }
 
 export function errorMessage(message: string) {
-  toast.error(message, {
-    position: 'top-right',
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'light',
-  })
+  toast.error(message, props)
+}
+
+export function warnMessage(message: string) {
+  toast.warn(message, props)
 }
